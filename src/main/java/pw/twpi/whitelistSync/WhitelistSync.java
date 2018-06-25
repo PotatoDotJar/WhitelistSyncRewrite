47194436569aa9871818b2585df6387bd1ae7237
PotatoSauceVFX <rj@potatosaucevfx.com>
2018-06-25 01:16:25 -0400

    Version

--- src/main/java/pw/twpi/whitelistSync/WhitelistSync.java
+++ src/main/java/pw/twpi/whitelistSync/WhitelistSync.java
@@ -42,7 +42,7 @@ import pw.twpi.whitelistSync.util.ConfigHandler;
 public class WhitelistSync {
 
     public static final String MODID = "whitelistsync";
-    public static final String VERSION = "1.2-1.12.2"; // Change gradle build config too!
+    public static final String VERSION = "1.3-1.12.2"; // Change gradle build config too!
     public static String SERVER_FILEPATH;
     public static Configuration config;
 
